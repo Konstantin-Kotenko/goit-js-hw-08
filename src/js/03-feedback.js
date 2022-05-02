509477fc20f9f76101d9b039e7d1518ad3445092
Konstantin-Kotenko <kostya.kotenkoo@gmail.com>
2022-05-02 17:46:57 +0300

    3task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,33 @@
+import throttle from 'lodash.throttle';
+
 const form = document.querySelector('.feedback-form');
+const LOCALSTORAGE = 'feedback-form-state';
+let data = {};
 
-const onInputForm = e => {};
+const onInputForm = e => {
+  data[e.target.name] = e.target.value;
+  const localStorageKey = JSON.parse(localStorage.getItem(LOCALSTORAGE));
+  localStorage.setItem(LOCALSTORAGE, JSON.stringify({ ...localStorageKey, ...data }));
+};
 
-form.addEventListener('input', onInputForm);
+const onSubmitForm = e => {
+  e.prevantDefault();
+  console.log(JSON.parse(localStorage.getItem(LOCALSTORAGE)));
+  e.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE);
+  data = {};
+};
+
+const savedData = () => {
+  const savedDataText = JSON.parse(localStorage.getItem(LOCALSTORAGE));
+  if (savedDataText?.email) {
+    form.email.value = savedDataText.email;
+  }
+  if (savedDataText?.message) {
+    form.message.value = savedDataText.message;
+  }
+};
+savedData();
+
+form.addEventListener('input', throttle(onInputForm, 500));
+form.addEventListener('submit', onSubmitForm);
